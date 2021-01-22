5e66a4806195d65eb70fa7bf3b51ecb85d3591df
Caleigh Christensen <christensen.caleigh@gmail.com>
2021-01-22 13:45:03 -0600

    first commit

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+
+        double pi = 3.14159;
+//        System.out.println("The value of pi is approximately: " + pi);
+        System.out.printf("The value of pi is approximately: %.2f%n", pi );
+
+//        Explore the Scanner Class
+
+//        #1
+        Scanner scanner = new Scanner(System.in);
+
+//        #2
+
+//        #3
+
+    }
+}
