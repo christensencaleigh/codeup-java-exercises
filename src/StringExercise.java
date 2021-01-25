9c7857527f53ebf98e2530e021a78c1b904f7b54
Caleigh Christensen <christensen.caleigh@gmail.com>
2021-01-25 11:44:32 -0600

    first commit

---
+++ src/StringExercise.java
@@ -0,0 +1,9 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String str1 = "We don't need no education";
+        System.out.println(str1);
+        str1 = "We don't need no thought control";
+        System.out.println(str1);
+
+    }
+}
