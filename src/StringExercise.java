c21394fe1a65ce9ad65ab8a86c0c6086a8d2ec29
Caleigh Christensen <christensen.caleigh@gmail.com>
2021-01-25 11:58:17 -0600

    first 2 problems done

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,9 +1,15 @@
 public class StringExercise {
     public static void main(String[] args) {
+        // 1
         String str1 = "We don't need no education";
         System.out.println(str1);
         str1 = "We don't need no thought control";
         System.out.println(str1);
 
+        // 2
+        String str2 = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(str2);
+
+        // 3
     }
 }
