be2d53a5c0f9a51bd819d9c7b95edb639895d59e
Caleigh Christensen <christensen.caleigh@gmail.com>
2021-01-25 15:58:26 -0600

    completed 2nd part of string exercise

--- src/Bob.java
+++ src/Bob.java
@@ -2,17 +2,25 @@ import java.util.Scanner;
 
 public class Bob {
     public static void main(String[] args) {
+//        Bob is a lackadaisical teenager. In conversation, his responses are very limited.
+//        Bob answers 'Sure.' if you ask him a question. (the input ends with a question mark)
+//        He answers 'Whoa, chill out!' if you yell at him. (the input ends with an exclamation mark)
+//        He says 'Fine. Be that way!' if you address him without actually saying anything. (empty input)
+//        He answers 'Whatever.' to anything else.
+
         Scanner scan = new Scanner(System.in);
         System.out.println("Ask a question or say something to Bob!!!");
-        String userInput = scan.nextLine();
-        boolean question = userInput.indexOf("?") != -1;
-        boolean exclamation = userInput.indexOf("!") != -1;
+        String userInput = scan.nextLine().trim(); //ensure that there is no whitespace to ensure correct response
 
 
-        if(question) {
+        if(userInput.endsWith("?")) {
             System.out.println("Sure.");
-        } else if(exclamation) {
+        } else if(userInput.endsWith("!")) {
             System.out.println("Whoa, chill out!");
+        } else if(userInput.isEmpty()) {
+            System.out.println("Fine. Be that way!");
+        } else {
+            System.out.println("Whatever.");
         }
     }
 }
