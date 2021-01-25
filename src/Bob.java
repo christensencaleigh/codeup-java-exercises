5daf75b95069f11952901fe79b20fd45c7062001
Caleigh Christensen <christensen.caleigh@gmail.com>
2021-01-25 12:46:42 -0600

    first commit

--- src/Bob.java
+++ src/Bob.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Bob {
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Ask a question or say something to Bob!!!");
+        String userInput = scan.nextLine();
+        boolean question = userInput.indexOf("?") != -1;
+        boolean exclamation = userInput.indexOf("!") != -1;
+
+
+        if(question) {
+            System.out.println("Sure.");
+        } else if(exclamation) {
+            System.out.println("Whoa, chill out!");
+        }
+    }
 }
