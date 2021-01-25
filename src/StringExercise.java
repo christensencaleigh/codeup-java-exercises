809b611f8759c900737d975013cd682f98c98590
Caleigh Christensen <christensen.caleigh@gmail.com>
2021-01-25 15:27:10 -0600

    fixed a problem, working nicely woot woot

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,9 +1,7 @@
 public class StringExercise {
     public static void main(String[] args) {
         // 1
-        String str1 = "We don't need no education";
-        System.out.println(str1);
-        str1 = "We don't need no thought control";
+        String str1 = "We don't need no education \nWe don't need no thought control";
         System.out.println(str1);
 
         // 2
